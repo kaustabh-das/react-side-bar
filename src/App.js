a932e75ea89553bd505c17292f2891d46079bd73
Kaustabh Kumar Das <kaustabhkumar1997@gmail.com>
2020-10-19 22:58:19 +0530

    0.2

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Products from './pages/Products';
+import Reports from './pages/Reports';
 
 function App() {
   return (
@@ -12,7 +15,9 @@ function App() {
       <Router>
         <Navbar />
         <Switch>
-          <Route path="/" /> 
+          <Route path="/" exact component={Home} />
+          <Route path="/products" component={Products} />
+          <Route path="/reports" component={Reports} /> 
         </Switch>
       </Router>
     </>
